0e8c131ef282455f5097f9792e29bfd89ebfa8ed
Wesley Pereira Claudio <135704231+wesleypereirac@users.noreply.github.com>
2024-08-26 09:09:37 -0300

    Atualizar o urls.php

--- loja/urls.php
+++ loja/urls.php
@@ -5,4 +5,19 @@ $uri = $_SERVER["REQUEST_URI"];
 $uri_handler = [
     "/loja/" => ""
 ]
+
+if ( @$_POST['username'] && @!$_COOKIE['username']){
+  $nome = $_POST['username'];
+  $nome_cookie = $_COOKIE['username'];
+  $senha = $_POST['password'];
+  $tipo = (isset($_POST['type'])) ? $_POST['type'] : "normal";
+
+  setcookie('username', $nome);
+  criar_cadastro($nome, $senha, $tipo);
+  header("Location: index.php");
+  //indica local de redirecionamento
+
+  exit();
+}
+
 ?>
