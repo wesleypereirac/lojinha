300f55bb2336195c8fffe28051b5f74a190313b6
Wesley Pereira Claudio <135704231+wesleypereirac@users.noreply.github.com>
2024-08-26 09:35:21 -0300

    Atualizar o urls.php
    mudanças drasticas no urls.php

--- loja/urls.php
+++ loja/urls.php
@@ -1,34 +1,42 @@
 <?php
-$uri = $_SERVER["REQUEST_URI"];
+$current_uri = $_SERVER["REQUEST_URI"];
 
 
 $uri_handler = [
-    "/loja/" => ""
+    "/loja/" => login_handler(),
+    "/loja/post/" => create_login()
 ]
 
+$uri_handler[$current_uri];
+//funções
 
-//main
-if ( @$_POST['username'] && @!$_COOKIE['username']){
-  $nome = $_POST['username'];
-  $nome_cookie = $_COOKIE['username'];
-  $senha = $_POST['password'];
-  $tipo = (isset($_POST['type'])) ? $_POST['type'] : "normal";
+function login_handler(){
+    
 
-  setcookie('username', $nome);
-  criar_cadastro($nome, $senha, $tipo);
-  header("Location: index.php");
+    if (@$_COOKIE['username']){
+        require "templates/index.html";
+        echo "<br><br>bem vindo (a), ". $_COOKIE['username'];
+    }
+    else{
+        require "templates/login.html";
+    }
+}
+
+
+function create_login(){
+    if ($_POST['username'] && @!$_COOKIE['username']){
+        $nome = $_POST['username'];
+        $nome_cookie = $_COOKIE['username'];
+        $senha = $_POST['password'];
+        $tipo = (isset($_POST['type'])) ?   $_POST['type'] : "normal";
+
+        setcookie('username', $nome);
+        criar_cadastro($nome, $senha, $tipo);
+        header("Location: index.php");
   //indica local de redirecionamento
 
-  exit();
+        exit();
+    }
 }
-
-if (@$_COOKIE['username']){
-  require "templates/index.html";
-  echo "<br><br>bem vindo (a), ", $_COOKIE['username'];
-}
-else{
-require "templates/login.html";
-}
-
 //abaixo codigos referentes ao redirecionamento
 ?>
